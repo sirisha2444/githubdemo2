4886a4d8e6d16010f5c67ffe843549c18e71cbb4
siris <siris@192.168.0.45>
2023-10-18 15:13:50 -0500

    first commit through eclipse

---
+++ githubdemo2/src/main/java/loginpage/Loginpage.java
@@ -0,0 +1,20 @@
+package loginpage;
+
+public class Loginpage {
+
+
+
+
+	public class baseclass  {
+		
+		public void testcase1() {
+			
+			
+			System.out.println("this is test cases1");
+			
+			System.out.println("This is my second commit");
+			
+		}
+
+	}
+}
---
+++ githubdemo2/src/test/java/logintest/logintest.java
@@ -0,0 +1,22 @@
+package logintest;
+
+
+public class logintest {
+	
+
+	public void testcase1() {
+		System.out.println("This is testcase1");
+		
+	}
+
+	public void testcase2() {
+		System.out.println("This is testcase2");
+		
+	}
+
+	public void testcase3() {
+		System.out.println("This is testcase3");
+		
+	}
+	}
+
